18bf953e345890930c04b88c58776b37bbc95084
Xin Yu <scaner@gmail.com>
2013-01-27 22:30:18 +0800

    netcdf create & output in Griddings. unfinished.

--- src/main/java/ep/common/Griddings.java
+++ src/main/java/ep/common/Griddings.java
@@ -2,12 +2,20 @@ package ep.common;
 
 
 import java.io.IOException;
+import java.util.Map;
 
 import ucar.ma2.Array;
+import ucar.ma2.DataType;
 import ucar.ma2.InvalidRangeException;
+import ucar.nc2.Attribute;
+import ucar.nc2.Dimension;
 import ucar.nc2.NetcdfFile;
+import ucar.nc2.NetcdfFileWriter;
+import ucar.nc2.Variable;
 
 public class Griddings {
+
+
   public static Gridding read(NetcdfFile ncfile, String variable) throws IOException, InvalidRangeException {
     Array array = ncfile.readSection(variable);
 
@@ -86,5 +94,83 @@ public class Griddings {
         destArr[od2] += srcArr[os];
       }
     }
+  }
+
+
+  public static NetcdfFileWriter createNetCDF(String path) throws IOException {
+    NetcdfFileWriter ncwriter = NetcdfFileWriter.createNew(
+      NetcdfFileWriter.Version.netcdf3, path);
+    return ncwriter;
+  }
+
+
+  public static void setAttributes(NetcdfFileWriter ncwriter, Map<String, Object> attrs) {
+    for (Map.Entry e: attrs.entrySet()) {
+      Attribute attr = new Attribute(e.getKey().toString(), e.getValue().toString());
+      ncwriter.addGroupAttribute(null, attr);
+    }
+  }
+
+
+  public static void addGlobalAttribute(NetcdfFileWriter ncwriter, String name, String value) {
+    Attribute attr = new Attribute(name, value);
+    ncwriter.addGroupAttribute(null, attr);
+  }
+
+
+  public static final String CONTACT = "qzhang@tsinghua.edu.cn";
+  public static final String CONVENTIONS = "CF-1.0";
+
+
+  public static void standardizeCF(NetcdfFileWriter ncwriter, int shape[]) throws IOException, InvalidRangeException {
+
+    addGlobalAttribute(ncwriter, "Convensions", CONVENTIONS);
+    addGlobalAttribute(ncwriter, "contact", CONTACT);
+
+    // int shape[] = g.getShape();
+
+    int resLat = shape[0];
+    int resLon = shape[1];
+
+    // add dimensions
+    Dimension latDim = ncwriter.addDimension(null, "lat", resLat);
+    Dimension lonDim = ncwriter.addDimension(null, "lon", resLon);
+
+    // add dimension variables
+    Variable latVar = ncwriter.addVariable(null, "lat", DataType.FLOAT, latDim.getName());
+    latVar.addAttribute(new Attribute("standard_name", "latitude"));
+    latVar.addAttribute(new Attribute("long_name", "latitude"));
+    latVar.addAttribute(new Attribute("units", "degrees_north"));
+    latVar.addAttribute(new Attribute("comment", "center_of_cell"));
+
+    Variable lonVar = ncwriter.addVariable(null, "lon", DataType.FLOAT, lonDim.getName());
+    lonVar.addAttribute(new Attribute("standard_name", "longitude"));
+    lonVar.addAttribute(new Attribute("long_name", "longitude"));
+    lonVar.addAttribute(new Attribute("units", "degrees_east"));
+    lonVar.addAttribute(new Attribute("comment", "center_of_cell"));
+
+    ncwriter.create();
+
+
+    Array latArr = Array.factory(float.class, new int[] {resLat});
+    float[] latArr2 = (float[])latArr.getStorage();
+    float cellLat = 180f / resLat;
+    float baseLat = -90f +  cellLat / 2;
+    for (int i = 0; i < resLat; ++i) {
+      latArr2[i] = baseLat;
+      baseLat += cellLat;
+    }
+    ncwriter.write(latVar, latArr);
+
+
+    Array lonArr = Array.factory(float.class, new int[] {resLon});
+    float[] lonArr2 = (float[])lonArr.getStorage();
+    float cellLon = 360f / resLon;
+    float baseLon = 0f +  cellLon / 2;
+    for (int i = 0; i < resLon; ++i) {
+      lonArr2[i] = baseLon;
+      baseLon += cellLon;
+    }
+    ncwriter.write(lonVar, lonArr);
   }
 }
