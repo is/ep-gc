f65d9b3507e10a678dd81f46718a69cb906d2855
Xin Yu <scaner@gmail.com>
2013-01-27 15:34:26 +0800

    simple & ugly combine remap function.

--- src/main/java/ep/common/Griddings.java
+++ src/main/java/ep/common/Griddings.java
@@ -27,4 +27,64 @@ public class Griddings {
     g.globalize();
     return g;
   }
+
+
+  public void remap(Gridding source, Gridding dest) {
+    // Very simple & ugly remap function.
+    // Only work on Globe surface combination
+    Array srcSur = source.getSurface();
+    Array destSur = dest.getSurface();
+
+    int srcShape[] = srcSur.getShape();
+    int destShape[] = destSur.getShape();
+
+    int srcResLat = srcShape[0];
+    int srcResLon = srcShape[1];
+
+    int destResLat = destShape[0];
+    int destResLon = destShape[1];
+
+    double srcDeltaLat = 180.0 / srcResLat;
+    double srcDeltaLon = 360.0 / srcResLon;
+
+    double destDeltaLat = 180.0 / destResLat;
+    double destDeltaLon = 360.0 / destResLon;
+
+    double srcBaseLat = - srcDeltaLat * 0.5;
+    double srcBaseLon;
+
+    double destLimitLat = destDeltaLat;
+    double destLimitLon;
+
+    float[] srcArr = (float[])srcSur.getStorage();
+    float[] destArr = (float[])destSur.getStorage();
+
+    int os = -1;
+    int od = 0;
+
+    for (int j = 0; j < srcResLat; ++j) {
+      srcBaseLat += srcDeltaLat;
+      if (srcBaseLat > destLimitLat) {
+        od += destResLon;
+        destLimitLat += destDeltaLat;
+      }
+
+
+      // reset scan line
+      srcBaseLon = - srcDeltaLon * 0.5;
+      destLimitLon = destDeltaLon;
+      int od2 = od;
+
+      for (int i = 0; i < srcResLon; ++i) {
+        os += 1;
+        srcBaseLon += srcDeltaLon;
+
+        if (srcBaseLon > destLimitLon) {
+          destLimitLon += destDeltaLon;
+          od2 += 1;
+        }
+        destArr[od2] += srcArr[os];
+      }
+    }
+  }
 }
