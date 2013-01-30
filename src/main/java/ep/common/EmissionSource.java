245e778ef891adde91611e0c9ae41c74feff86f5
Xin Yu <scaner@gmail.com>
2013-01-31 00:24:22 +0800

    rename EmissionDataSets to EmissionSource

--- src/main/java/ep/common/EmissionSource.java
+++ src/main/java/ep/common/EmissionSource.java
@@ -3,6 +3,6 @@ package ep.common;
 
 import ucar.ma2.Array;
 
-public interface EmissionDataSet {
+public interface EmissionSource {
   Array getSurface(String dsname, String sector, String species);
 }
