31068d96ea973677f00c38c5f32b2683eaac345e
Xin Yu <scaner@gmail.com>
2013-02-04 00:45:58 +0800

    add details in e.c.Configuration

--- src/main/java/ep/common/Configuration.java
+++ src/main/java/ep/common/Configuration.java
@@ -1,5 +1,21 @@
 package ep.common;
 
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Configuration {
   public String root;
+
+  public String species[];
+  public String sectors[];
+  public String emissions[];
+
+  public Map<String, EmissionSourceConfig> emissionConfig;
+
+  @JsonIgnore
+  public Map<String, EmissionSource> emissionSource;
 }
