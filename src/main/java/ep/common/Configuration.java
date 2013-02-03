b30d1b4d675a90244c3af98d4f92ddbebcf719f2
Xin Yu <scaner@gmail.com>
2013-02-03 21:18:29 +0800

    clear code.

--- src/main/java/ep/common/Configuration.java
+++ src/main/java/ep/common/Configuration.java
@@ -1,15 +1,5 @@
 package ep.common;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.charset.Charset;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class Configuration {
   public String root;
-
 }
