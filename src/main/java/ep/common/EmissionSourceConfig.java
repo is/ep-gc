0e555e987f5d33c85d9445c27ec847b3cef9ab33
Xin Yu <scaner@gmail.com>
2013-02-04 00:44:20 +0800

    emission source config base class.

---
+++ src/main/java/ep/common/EmissionSourceConfig.java
@@ -0,0 +1,21 @@
+package ep.common;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({
+  @JsonSubTypes.Type(value = EmissionSourceConfig.class, name = "base"),
+  @JsonSubTypes.Type(value = FsEmissionSourceConfig.class, name = "fs")
+})
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class EmissionSourceConfig {
+  @JsonIgnore
+  public boolean enable;
+
+  public String name;
+  public String dateStep;
+  public String factorArray;
+}
