857b80c4d273a65eeefde4d731b0391a3bc5de00
Xin Yu <scaner@gmail.com>
2013-02-03 13:42:46 +0800

    ESID - Emission Surface ID.

---
+++ src/main/java/ep/common/ESID.java
@@ -0,0 +1,15 @@
+package ep.common;
+
+public class ESID {
+  public String name;
+  public String date;
+  public String sector;
+  public String species;
+
+  public ESID(String name, String date, String sector, String species) {
+    this.name = name;
+    this.date = date;
+    this.sector = sector;
+    this.species = species;
+  }
+}
--- src/main/java/ep/common/EmissionSource.java
+++ src/main/java/ep/common/EmissionSource.java
@@ -4,5 +4,5 @@ package ep.common;
 import ucar.ma2.Array;
 
 public interface EmissionSource {
-  Array getSurface(String dsname, String sector, String species);
+  Array getArray(ESID esid);
 }
