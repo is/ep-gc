c971230622108cd89eeb9c95adc5e3775ea75bc8
Xin Yu <scaner@gmail.com>
2013-02-04 22:45:34 +0800

    upgrade CJS demo.

--- src/main/java/ep/geoschem/demo/ConfigJacksonSerializer.java
+++ src/main/java/ep/geoschem/demo/ConfigJacksonSerializer.java
@@ -8,12 +8,15 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import ep.geoschem.Configuration;
 import ep.common.EmissionSourceConfig;
 import ep.common.FsEmissionSourceConfig;
+import ucar.ma2.InvalidRangeException;
 
 public class ConfigJacksonSerializer {
-  public static void main(String args[]) throws IOException {
+  public static void main(String args[]) throws IOException, InvalidRangeException {
     Configuration cf = new Configuration();
 
     cf.root = "data";
+    cf.conf = "conf";
+    cf.defaultEmission = "EDGAR";
     cf.emissions = new String[] {"EDGAR", "EMEP", "MEIC"};
     cf.sectors = new String[] {"power", "industry", "residential", "transporation", "agriculture"};
 
@@ -25,10 +28,13 @@ public class ConfigJacksonSerializer {
     esc1.name = "EDGAR";
     esc1.dateStep = "yearly";
     esc1.basePath = "data/in/edgar";
+    esc1.pathTemplate = "<cf.basePath>/<es.name>/<es.species>/" +
+      "v42_<es.species>_<es.date>_IPCC_<es.sector>.0.1x0.1.nc|||" +
+      "emi_<es.speciesLower>";
 
-    cf.emissionConfig = new HashMap<String, EmissionSourceConfig>();
-    cf.emissionConfig.put(esc0.name, esc0);
-    cf.emissionConfig.put(esc1.name, esc1);
+    cf.emissionConfigs = new HashMap<String, EmissionSourceConfig>();
+    cf.emissionConfigs.put(esc0.name, esc0);
+    cf.emissionConfigs.put(esc1.name, esc1);
 
     ObjectMapper om = new ObjectMapper();
     om.configure(SerializationFeature.INDENT_OUTPUT, true);
@@ -37,6 +43,9 @@ public class ConfigJacksonSerializer {
     System.out.println(cfStr);
     Configuration c2 = om.readValue(cfStr,Configuration.class);
 
-    System.out.println(c2.emissionConfig.get("EDGAR").name);
+    System.out.println(c2.emissionConfigs.get("EDGAR").name);
+    c2.setup();
+
+    System.out.println(c2);
   }
 }
