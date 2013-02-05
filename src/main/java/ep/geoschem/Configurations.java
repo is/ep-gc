913865ff39d54eb4c49bc0513de2e2fada7b90c8
Xin Yu <scaner@gmail.com>
2013-02-05 21:49:55 +0800

    improve json configuration load function

--- src/main/java/ep/geoschem/Configurations.java
+++ src/main/java/ep/geoschem/Configurations.java
@@ -7,27 +7,33 @@ import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Configurations {
+//  public static Configuration loadFile(File filePath) throws IOException {
+//    FileInputStream fis = new FileInputStream(filePath);
+//    String json = null;
+//
+//    try {
+//      FileChannel fc = fis.getChannel();
+//      MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+//      json = Charset.forName("utf-8").decode(bb).toString();
+//    } finally {
+//      fis.close();
+//    }
+//
+//    if (null == json)
+//      return null;
+//
+//    return load(json);
+//  }
+
   public static Configuration load(File filePath) throws IOException {
-    FileInputStream fis = new FileInputStream(filePath);
-    String json = null;
-
-    try {
-      FileChannel fc = fis.getChannel();
-      MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
-      json = Charset.forName("utf-8").decode(bb).toString();
-    } finally {
-      fis.close();
-    }
-
-    if (null == json)
-      return null;
-
-    return load(json);
+    ObjectMapper mapper = new ObjectMapper();
+    Configuration cf = mapper.readValue(filePath, Configuration.class);
+    return cf;
   }
-
 
   public static Configuration load(String json) throws IOException {
     ObjectMapper mapper = new ObjectMapper();
