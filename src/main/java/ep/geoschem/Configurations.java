7c0d2ad4e9893c20e1411d5de4864f906d42d872
Xin Yu <scaner@gmail.com>
2013-02-08 19:40:58 +0800

    Clear code.

--- src/main/java/ep/geoschem/Configurations.java
+++ src/main/java/ep/geoschem/Configurations.java
@@ -1,13 +1,8 @@
 package ep.geoschem;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.charset.Charset;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Configurations {
