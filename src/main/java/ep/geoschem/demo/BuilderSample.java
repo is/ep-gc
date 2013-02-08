792108e720dc9455dfd0315c23ddd5a4b2e2c225
Xin Yu <scaner@gmail.com>
2013-02-08 23:53:30 +0800

    Upgrade e.g.d.BuilderSample

--- src/main/java/ep/geoschem/demo/BuilderSample.java
+++ src/main/java/ep/geoschem/demo/BuilderSample.java
@@ -1,17 +1,14 @@
 package ep.geoschem.demo;
 
+import java.util.HashMap;
+
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import ep.common.EmissionSourceConfig;
 import ep.common.FileSystemEmissionSourceConfig;
 import ep.geoschem.Configuration;
 import ep.geoschem.Target;
 import ep.geoschem.builder.DataSetBuilder;
-import ucar.ma2.InvalidRangeException;
-
-import java.io.IOException;
-import java.util.HashMap;
 
 public class BuilderSample {
   public static void main(String args[]) throws Exception {
@@ -27,8 +24,8 @@ public class BuilderSample {
     esc0.name = "EMEP";
     esc0.dateStep = "yearly";
     esc0.basePath = "data/in";
-    esc0.pathTemplate = "<cf.basePath>/<es.nameLower>/EMEP_CO_SOx_NH3_NOx_NMVOC_<es.date>_0.5x0.5.nc|||<es.species>_<es.sector>";
-    esc0.speciesAliases = new HashMap<String, String>();
+    esc0.pathTemplate = "<cf.basePath>/<es.nameLower>/EMEP_CO_SOx_NH3_NOx_NMVOC_<es.year>_0.5x0.5.nc|||<es.species>_<es.sector>";
+    esc0.speciesAliases = new HashMap<>();
     esc0.speciesAliases.put("SO2", "SOx");
 
     FileSystemEmissionSourceConfig esc1 = new FileSystemEmissionSourceConfig();
@@ -36,10 +33,10 @@ public class BuilderSample {
     esc1.dateStep = "yearly";
     esc1.basePath = "data/in";
     esc1.pathTemplate = "<cf.basePath>/<es.nameLower>/<es.species>/" +
-      "v42_<es.species>_<es.date>_IPCC_<es.sector>.0.1x0.1.nc|||" +
+      "v42_<es.species>_<es.year>_IPCC_<es.sector>.0.1x0.1.nc|||" +
       "emi_<es.speciesLower>";
 
-    cf.emissionConfigs = new HashMap<String, EmissionSourceConfig>();
+    cf.emissionConfigs = new HashMap<>();
     cf.emissionConfigs.put(esc0.name, esc0);
     cf.emissionConfigs.put(esc1.name, esc1);
 
@@ -56,7 +53,6 @@ public class BuilderSample {
     c2.init();
 
     System.out.println(c2);
-    String ss[] = c2.getSourceSectors("NH3", "Power", "EDGAR");
     System.out.println(c2.getSourceSectors("NH3", "Power", "EDGAR"));
     System.out.println(c2.getYearIndex("EMEP", "1974"));
     System.out.println(c2.getYearIndex("EMEP", "2008"));
@@ -70,7 +66,7 @@ public class BuilderSample {
     target.base = "data/o0";
     target.dateStep = "monthly";
     target.shape = new int[] {180, 360};
-    target.enabled = new String[] {"EDGAR", "EMEP", };
+    target.enabled = new String[] {"EDGAR", };
     target.pathTemplate = "<ta.base>/<es.name>/<es.date>.nc|||<es.species>_<es.sector>";
     target.init();
 
