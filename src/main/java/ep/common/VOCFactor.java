d08f9ca61b717b801e87e7b4e4dd184289ef980b
Xin Yu <scaner@gmail.com>
2013-02-09 23:07:44 +0800

    Update factor classes.

--- src/main/java/ep/common/VOCFactor.java
+++ src/main/java/ep/common/VOCFactor.java
@@ -11,48 +11,43 @@ import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.google.common.base.Strings;
 
-public class ScalarMonthlyTimeFactor implements GridFactor {
-
-  public Map<String, float[]> factors;
+public class VOCFactor implements GridFactor {
+  public Map<String, Float> vocMap;
 
   public void loadFromCSV(File fin) throws IOException {
     CsvSchema schema = CsvSchema.emptySchema().withHeader();
     ObjectMapper mapper = new CsvMapper();
-    factors = new HashMap<>();
 
+    vocMap = new HashMap<>();
     MappingIterator<Map<String, String>> it = mapper.reader(Map.class).
       with(schema).readValues(fin);
 
     while(it.hasNext()) {
       Map<String, String> row = it.next();
       String es = row.get("SOURCE");
-      String sp = row.get("SPECIES");
       String st = row.get("SECTOR");
 
-      if (Strings.isNullOrEmpty(es))
+      if (Strings.isNullOrEmpty(es) || Strings.isNullOrEmpty(st))
         continue;
 
-      String key = es + "," + sp + "," + st;
-      float farray[] = new float[12];
+      for (Map.Entry<String, String> e: row.entrySet()) {
+        if (e.getKey().equals("SOURCE") || e.getKey().equals("SECTOR"))
+          continue;
+        if (e.getKey().startsWith("__"))
+          continue;
 
-      for (int i = 0; i < 12; i++) {
-        farray[i] = Float.parseFloat(row.get(Integer.toString(i + 1)));
+        String key = es + "," + st + "," + e.getKey();
+        vocMap.put(key, Float.parseFloat(e.getValue()));
       }
-      factors.put(key, farray);
     }
   }
 
-
-  @Override
   public void apply(ESID esid, Grid g) {
-    String key = esid.name + "," + esid.species + "," + esid.sector;
-    int month = Integer.parseInt(esid.date.substring(4, 6));
-
-    float farray[] = factors.get(key);
-    if (farray == null)
+    Float F = vocMap.get(esid.name + "," + esid.species);
+    if (F == null)
       return;
 
-    float  factor = farray[month - 1];
-    g.floatScale(factor);
+    g.floatScale(F);
+
   }
 }
