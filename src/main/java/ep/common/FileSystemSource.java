47da46e60851ad3e09ad8f817ad4da89eadadfae
Xin Yu <scaner@gmail.com>
2013-02-12 13:03:36 +0800

    Add voc mapping.

--- src/main/java/ep/common/FileSystemSource.java
+++ src/main/java/ep/common/FileSystemSource.java
@@ -8,22 +8,22 @@ import org.stringtemplate.v4.ST;
 import ucar.ma2.InvalidRangeException;
 
 public class FileSystemSource implements Source {
-  FileSystemSourceConfig conf;
+  FileSystemSourceConfig cf;
   ST pathSTTemplate;
   Splitter pathSplitter;
   Grid factoryArray;
 
 
-  public FileSystemSource(FileSystemSourceConfig conf) throws IOException, InvalidRangeException {
-    this.conf = conf;
+  public FileSystemSource(FileSystemSourceConfig cf) throws IOException, InvalidRangeException {
+    this.cf = cf;
     this.pathSplitter = Splitter.on("|||");
 
-    this.pathSTTemplate = new ST(conf.pathTemplate);
-    this.pathSTTemplate.add("cf", conf);
+    this.pathSTTemplate = new ST(cf.pathTemplate);
+    this.pathSTTemplate.add("cf", cf);
     this.pathSTTemplate.add("es", ST.EMPTY_ATTR);
 
-    if (this.conf.factorArray != null) {
-      factoryArray = Grids.read(this.conf.factorArray);
+    if (this.cf.factorArray != null) {
+      factoryArray = Grids.read(this.cf.factorArray);
     }
   }
 
@@ -36,9 +36,17 @@ public class FileSystemSource implements Source {
 
   @Override
   public Grid getGridding(ESID esid) throws Exception {
-    if (null != conf.speciesAliases && conf.speciesAliases.get(esid.species) != null) {
+    if (null != cf.speciesAliases && cf.speciesAliases.get(esid.species) != null) {
       esid = new ESID(esid);
-      esid.species = conf.speciesAliases.get(esid.species);
+      esid.species = cf.speciesAliases.get(esid.species);
+    }
+
+    ESID oesid = esid;
+    boolean isVoc = cf.vocFactor.isVoc(esid.species);
+
+    if (isVoc) {
+      esid = new ESID(esid);
+      esid.species = cf.vocFactor.species;
     }
 
     String arrayPath = randerPath(esid);
@@ -47,8 +55,12 @@ public class FileSystemSource implements Source {
       g.floatScale(factoryArray);
     }
 
-    if (esid.date != null && esid.date.length() == 6 && conf.timeFactor != null) {
-      conf.timeFactor.apply(esid, g);
+    if (esid.date != null && esid.date.length() == 6 && cf.timeFactor != null) {
+      cf.timeFactor.apply(esid, g);
+    }
+
+    if (isVoc) {
+      cf.vocFactor.apply(oesid, g);
     }
     return g;
   }
