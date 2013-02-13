257c56110280584789c723173e1fcc35095f9973
Xin Yu <scaner@gmail.com>
2013-02-13 16:50:31 +0800

    Simplify name of GenDataSet1 to GD1.

--- src/main/java/ep/geoschem/demo/gen/GD1.java
+++ src/main/java/ep/geoschem/demo/gen/GD1.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 
 import ucar.ma2.InvalidRangeException;
 
-public class GenDataSet1 extends Generater {
+public class GD1 extends Generater {
   public static void main(String args[]) throws IOException, InvalidRangeException {
-    GenDataSet1 g = new GenDataSet1();
+    GD1 g = new GD1();
     g.init("1");
     g.gen();
   }
