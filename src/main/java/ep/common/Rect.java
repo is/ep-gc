9775454c82cb7b3e1630e8978e41b38fb58bde60
Xin Yu <scaner@gmail.com>
2013-03-26 23:33:28 +0800

    Add rect class

---
+++ src/main/java/ep/common/Rect.java
@@ -0,0 +1,16 @@
+package ep.common;
+
+public class Rect {
+  public int left;
+  public int bottom;
+  public int width;
+  public int height;
+
+
+  public Rect(int left, int bottom, int width, int height) {
+    this.left = left;
+    this.bottom = width;
+    this.width = width;
+    this.height = height;
+  }
+}
